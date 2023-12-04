546053c7fbeb025f2f42e60634dfb67769c66f4a
philipsoitu <95496892+philipsoitu@users.noreply.github.com>
2023-12-04 17:14:12 -0500

    testing button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+    let name="yo";
+
+    function addToName(){
+        name = name + "yo";
+    }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>{name}</p>
+<button on:click={addToName}>
+    Add to name
+</button>
